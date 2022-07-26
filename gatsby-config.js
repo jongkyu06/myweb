695ca28edfd19175f7c252e92b8b264d0ca6e539
hjk <gwr890klol@gmail.com>
2022-07-26 13:31:56 +0900

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `난 모르겠다.....`,
     author: {
-      name: `Kyle Mathews`,
+      name: `동아리`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
